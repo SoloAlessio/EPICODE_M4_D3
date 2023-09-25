35aacb373f70b2a4ec6fdbace18ba171fc98264e
Alessio <belcastroalessio@outlook.it>
2023-09-25 20:41:31 +0200

    IMPLEMENTATA LA BARRA DI RICERCA

--- java.js
+++ java.js
@@ -1,41 +1,55 @@
-const fetchSearch = (query) => {
-    fetch("https://api.pexels.com/v1/search?query=" + query, {
-        headers: { 'Authorization': 'RClWoemV9W7o093YKulQBNUJKzGzDNmq32SvNTENnhSSBrvItb8Nwmor' }
+const Ricerca = document.querySelector("#searchField")
+
+function titoli(){
+  const titoli = document.querySelectorAll(".title")
+  const body = document.querySelector(".modal-body")
+
+  body.innerHTML = ""
+
+  for (let i = 0; i < titoli.length; i++) {
+    body.innerHTML += `<p>${titoli[i].innerHTML}</p>`
+  }
+}
+
+function secondsToMinutes(seconds) {
+
+  const minutes = Math.floor(seconds / 60);
+  let remainingSeconds = seconds % 60;
+
+  if(remainingSeconds < 10){
+    remainingSeconds = "0" + remainingSeconds
+  }
+
+  return `${minutes}:${remainingSeconds}`;
+}
+
+const search = (query) => {
+  const section = document.querySelector(`#searchResults`)
+  const row = document.querySelector("#searchResults .row")
+  row.innerHTML = ""
+
+  fetch(
+    "https://striveschool-api.herokuapp.com/api/deezer/search?q=" + query
+  )
+    .then((raw) => {
+      return raw.json()
     })
-        .then((raw) => {
-            return raw.json();
-        })
-        .then((res) => {
-            let cars = res.photos;
-            let row = document.querySelector(".row");
+    .then((res) => {
+      let music = res.data
+      section.style.display = "block"
+      for (let i = 0; i < music.length; i++) {
+        const element = music[i]
+        row.innerHTML += `<div class='col col-3 mt-4'> 
+                            <img class='w-100 rounded' src='${element.album.cover_xl}'/> 
+                            <p class="mt-3 mb-0 d-flex justify-content-between title">
+                              <span>${element.title}</span>
+                              <span class="text-muted">${secondsToMinutes(element.duration)}</span>
+                            </p>
+                            <p class="text-white-50">${element.artist.name}</p>
+                          </div>`
+      }
+    })
 
-            if(cars.length < 1) {
-                row.innerHTML += `<div class='col'><p>Nessuna Immagine Trovata</p></div>`
-            } else{
-                for (let i = 0; i < cars.slice(0,3).length; i++) {
-                    const element = cars[i];
-                    console.log(element.src.large);
-                    row.innerHTML += `<div class='col d-flex justify-content-center'><img class='rounded shadow-lg' src='${element.src.large}'/></div>`
-    
-                }
-            }
-        })
-        .catch((err) => { alert(err); });
+    titoli()
 }
 
-function button() {
-    let valore = document.querySelector("#cerca").value;
-    if(valore != ""){
-        if (document.querySelector(".row") == "") {
-            fetchSearch(valore);   
-        } else{
-            document.querySelector(".row").innerHTML = "";
-            fetchSearch(valore);
-        }
-    } else{
-        alert("TESTO INSERITO NON VALIDO")
-    }
-    
-    
-    
-}
